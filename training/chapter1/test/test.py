def49f32fe9f74a272cc7fc40ee9bccbd18a97bb
AvinciClub NJ <avinciclubnj@gmail.com>
2018-06-09 11:05:43 -0400

    training chapter1 test

---
+++ training/chapter1/test/test.py
@@ -0,0 +1,12 @@
+"""
+ID: yezhang2
+LANG: PYTHON3
+TASK: test
+"""
+fin = open('test.in', 'r')
+fout = open('test.out', 'w')
+
+x,y = map(int, fin.readline().split())
+sum = x + y
+fout.write(str(sum) + '\n')
+fout.close()
