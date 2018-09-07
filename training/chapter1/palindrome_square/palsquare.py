9137b866a92df6ff079a3e01ffea8d19cd3aacf6
yezhang4528 <yezhang4528@gmail.com>
2018-09-06 20:35:46 -0400

    palindrom passed 3 cases

--- training/chapter1/palindrome_square/palsquare.py
+++ training/chapter1/palindrome_square/palsquare.py
@@ -4,14 +4,35 @@ LANG: PYTHON3
 TASK: palsquare
 '''
 import math
+def isPalindrome(inStr):
+    return inStr == inStr[::-1]
 
+def getNumInBase(input, base):
+    outStr = ""
+    while input > 0:
+        quotient = input//base
+        remainder = input % base
+        outStr = str(remainder) + outStr
+        input = quotient
+    return outStr
+    
 with open("palsquare.in", "r") as fin:
-    base = fin.readline().strip('\n')
+    base = int(fin.readline().strip('\n'))
 
-print(base)
+palSquareList = []
 for num in range(1, 301):
-    square = math.pow(num, 2)
-    print(square)
+    square = int(math.pow(num, 2))
+    if base != 10:
+        squareInStr = getNumInBase(square, base)
+    else:
+        squareInStr = str(square)
+    if isPalindrome(squareInStr):
+        if base == 10:
+            newPair = (str(num), squareInStr)
+        else:
+            newPair = (getNumInBase(num, base), squareInStr)
+        palSquareList.append(newPair)
 
-#x = math.floor(math.log(16,3))
-#print(x)
+with open("palsquare.out", "w") as fout:
+    for res in palSquareList:
+        fout.write(res[0] + ' ' + res[1] + '\n')
