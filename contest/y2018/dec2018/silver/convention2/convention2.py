8dabcfe6e819c9c7c90a94f7c5780d9d485c7082
Sarah Zhang <yz176j@att.com>
2020-01-19 21:16:07 -0500

    Dec2018 silver convention2 done

--- contest/y2018/dec2018/silver/convention2/convention2.py
+++ contest/y2018/dec2018/silver/convention2/convention2.py
@@ -1,37 +1,58 @@
+import bdb
+import sys
+from heapq import heappush, heappop, heapify
+
 cows = []
-with open("convention2.in", "r") as fin:
+if len(sys.argv) > 1:
+    inputFile = "data/%s.in" % sys.argv[1]
+else:
+    inputFile = "convention2.in"
+
+with open(inputFile, "r") as fin:
     totalCows = int(fin.readline().strip())
     for i in range(totalCows):
         arr, graze = map(int, fin.readline().split())
         cows.append((i, arr, graze))
 
 cows.sort(key=lambda x: x[1])
-print(cows)
+#print(cows)
 maxWait = 0
 cowInQ = []
-curTime = cows[0][1]
-pWait = cows[0][2] + curTime       # pasture pending time
+pWait = cows[0][2] + cows[0][1]       # pasture pending time
 
 def processQ(cows):
-    global pWait
-    cow = cows.pop()
-    if pWait > 0:
-        curTime += pWait
-        maxWait = max(maxWait, curTime - cow[1])
-    pWait = cow[2]
-    
-for cow in cows[1:]:
-    if pWait <= cow[1] and len(cowInQ) == 0:
-        pWait = cow[1] + cow[2]
-        continue
+    global pWait, maxWait
+    cow = heappop(cows)
+    wait = pWait - cow[1]
+    maxWait = max(maxWait, wait)
+    pWait = pWait + cow[2]
 
-    if pWait > cow[1]:
-        cowInQ.append(cow)
-        cowInQ.sort(key=lambda x: x[0])
-    else if len(cowInQ) == 0:
-        pWait = cow[1] + cow[2]
-    else:
+try:
+    #import pdb; pdb.set_trace()     # sarah, for debugging
+    heapify(cowInQ)
+    for cow in cows[1:]:
+        if pWait <= cow[1] and len(cowInQ) == 0:
+            # no wait
+            pWait = cow[1] + cow[2]
+            continue
+    
         # Pasture will be available before the cow arrives.
         # Process cows in Q.
         while pWait < cow[1] and len(cowInQ) > 0:
             processQ(cowInQ)
+    
+        if pWait >= cow[1]:
+            heappush(cowInQ, cow)
+        else:
+            # no wait
+            pWait = cow[1] + cow[2]
+
+    while len(cowInQ) > 0:
+        processQ(cowInQ)
+
+except Exception as e:
+    if isinstance(e, bdb.BdbQuit):
+        exit(0)
+
+with open("convention2.out", "w") as fout:
+    fout.write(str(maxWait) + '\n')
--- contest/y2018/dec2018/silver/convention2/heapq.py
+++ contest/y2018/dec2018/silver/convention2/heapq.py
@@ -1,9 +1,9 @@
 from heapq import heappush, heappop, heapify
 
 testQ = [
+    (2, 20, 50),
     (1, 25, 3),
     (8, 105, 30),
-    (2, 20, 50),
     (5, 10, 17),
     (0, 100, 10)
 ]
@@ -11,16 +11,20 @@ testQ = [
 print("Before sort")
 print(testQ)
 
+"""
 testQ.sort(key=lambda x: x[0])
-
 print("After sort")
 print(testQ)
+"""
+heapify(testQ)
+print(heappop(testQ))
 
 print("Push 3, 66, 5 to list")
-#heapify(testQ)     # Doesn't help
 heappush(testQ, (3, 66, 5))
-testQ.sort(key=lambda x:x[0])
-print(testQ)
+#testQ.sort(key=lambda x:x[0])
+
+while len(testQ) > 0:
+    print(heappop(testQ))
 
 l = [2, 6, 8]
 heappush(l, 5)
